2e4cde38408fbae181e9f2d9e8e068cefd37ae59
nayangoelsjsu <nayan.goel@sjsu.edu>
2017-04-01 22:08:14 -0700

    ParserCode Readfile fn
    ParserCode Readfile fn

--- src/ParserCode.java
+++ src/ParserCode.java
@@ -23,5 +23,46 @@ public class ParserCode {
 		return umlbuild;
 	}
 
+	public StringBuilder ReadFile(String file_path) throws FileNotFoundException
+	{
+		File f = new File(file_path);
+		umlbuild.append("@startuml\n skinparam classAttributeIconSize 0\n");
+		if(f.exists())
+		{
+			System.out.println("file is present");
+			System.out.println(f.listFiles().toString());
+			for(File f : f.listFiles())
+			{
+				if(f.getName().endsWith(".java"))
+				{
+					try 
+					{
+						CompilationUnit comp_unit = JavaParser.parse(f);
+						ClassOrInterfaceDeclaration class_inter_dec = GetClassAndInterface(comp_unit);
+						System.out.println(class_inter_dec+"----------");
+						if (class_inter_dec != null) {
+							CIMap.put(class_inter_dec.getName().toString(), class_inter_dec);
+							System.out.println("CIMAP : "+class_inter_dec.getName().toString());
+						}
+						parseClassInterfaceTypes(class_inter_dec);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			umlbuild.append("\n@enduml");
+            System.out.print( umlbuild.toString());
+		}
+		else
+		{
+			System.out.println("file does not exists");
+		}
+		
+		return umlbuild;
+		
+	}
+	
 
 }

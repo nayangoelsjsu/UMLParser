7a16c9758ef101757e01704da71dec2b8e4f486a
nayangoelsjsu <nayan.goel@sjsu.edu>
2017-04-08 17:05:21 -0700

    Parser_UML edit 1
    Parser_UML edit 1

--- src/Parser_UML.java
+++ src/Parser_UML.java
@@ -6,6 +6,9 @@ public class Parser_UML {
         if (args[0].equals("class")) {
             ParseEngine PEngine = new ParseEngine(args[1], args[2]);
             PEngine.start();
+        } else if (args[0].equals(("seq"))) {
+            ParseSeqEngine pSeqEngine = new ParseSeqEngine(args[1], args[2], args[3], args[4]);
+            pSeqEngine.start();
         } 
 
     }
